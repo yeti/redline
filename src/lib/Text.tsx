89b0110649a06a65cec4ab88139d4574cc65fb16
rezn5447 <rezn5447gmail.com>
2019-03-22 13:11:38 -0700

    include husky and prettier deeps

--- src/lib/Text.tsx
+++ src/lib/Text.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { black } from '../shared/styles/colors';
-import { StyleSheet, Text } from 'react-native';
-import { StyleArray } from 'types/shared';
+import * as React from "react";
+import { black } from "../shared/styles/colors";
+import { StyleSheet, Text } from "react-native";
+import { StyleArray } from "../shared/types";
 
 // Component
 // ----------------------------------------------------------------------------
@@ -14,7 +14,7 @@ interface Props {
 const TextComponent: React.SFC<Props> = ({
   children,
   extraStyles,
-  font,
+  font
 }: Props) => {
   /*  extraStyles will always be an object. Since we don't require extraStyles,
       ts thinks that the prop may be undefined, but we actually have a default
@@ -23,52 +23,46 @@ const TextComponent: React.SFC<Props> = ({
   const extraStylesCasted = extraStyles as StyleArray;
 
   return (
-    <Text
-      style={[
-        styles.text,
-        font && styles[font],
-        ...extraStylesCasted,
-      ]}
-    >
+    <Text style={[styles.text, font && styles[font], ...extraStylesCasted]}>
       {children}
     </Text>
   );
 };
 
 export enum Font {
-  SystemBold = 'SystemBold',
-  SystemRegular = 'SystemRegular',
-  SystemSemibold = 'SystemSemibold',
+  SystemBold = "SystemBold",
+  SystemRegular = "SystemRegular",
+  SystemSemibold = "SystemSemibold"
 }
 
 TextComponent.defaultProps = {
   extraStyles: [{}],
-  font: Font.SystemRegular,
+  font: Font.SystemRegular
 };
 
 // Styles
 // ----------------------------------------------------------------------------
-const systemFont = 'system font';
+const systemFont = "system font";
 
 const styles = StyleSheet.create({
   // fonts
   [Font.SystemRegular]: {
     fontFamily: systemFont,
-    fontWeight: '400',
+    fontWeight: "400"
   },
   [Font.SystemSemibold]: {
     fontFamily: systemFont,
-    fontWeight: '600',
+    fontWeight: "600"
   },
   [Font.SystemBold]: {
     fontFamily: systemFont,
-    fontWeight: '700',
+    fontWeight: "700"
   },
   //
   text: {
     fontSize: 12,
-    color: black,
-  },
+    color: black
+  }
 });
 
 export default TextComponent;
